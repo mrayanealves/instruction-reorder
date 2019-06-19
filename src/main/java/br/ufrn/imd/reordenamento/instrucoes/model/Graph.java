d9c187082a09cf0896ed64475b2779c03ed01ce5
Rayane <mrayalves05@gmail.com>
2019-06-19 15:44:36 -0300

    Adicionando coisas

--- src/main/java/br/ufrn/imd/reordenamento/instrucoes/model/Graph.java
+++ src/main/java/br/ufrn/imd/reordenamento/instrucoes/model/Graph.java
@@ -4,18 +4,42 @@ import java.util.List;
 
 public class Graph {
 	private Node root;
+	private Integer level;
 	private List<Graph> child;
 	
 	public Node getRoot() {
 		return root;
 	}
+	
 	public void setRoot(Node root) {
 		this.root = root;
 	}
+	
+	public Integer getLevel() {
+		return level;
+	}
+	
+	public void setLevel(Integer level) {
+		this.level = level;
+	}
+	
 	public List<Graph> getChild() {
 		return child;
 	}
+	
 	public void setChild(List<Graph> child) {
 		this.child = child;
+	}
+	
+	public void insertNode(Node newNode) {
+		
+	}
+	
+	public List<Graph> returnOrderedInstructions(Graph graph){
+		return null;
+	}
+	
+	private List<Graph> levelOrder(){
+		return null;
 	}
 }
---
+++ src/main/java/br/ufrn/imd/reordenamento/instrucoes/model/Queue.java
@@ -0,0 +1,35 @@
+package br.ufrn.imd.reordenamento.instrucoes.model;
+
+import java.util.List;
+
+public class Queue {
+	private List<Graph> graphs;
+
+	public Queue() {
+		super();
+	}
+	
+	/**
+	 * Adiciona elementos na fila
+	 * @param graph Graph a ser adicionado
+	 */
+	public void enqueue(Graph graph) {
+		this.graphs.add(graph);
+	}
+
+	/**
+	 * Remove elementos da fila
+	 * @return Graph que está no começo da fila
+	 */
+	public Graph dequeue() {
+		return this.graphs.remove(0);
+	}
+	
+	/**
+	 * Verifica se a fila está vazia
+	 * @return boolean que indica se está vazia
+	 */
+	public boolean isEmpty() {
+		return this.graphs.size() == 0;
+	}
+}
