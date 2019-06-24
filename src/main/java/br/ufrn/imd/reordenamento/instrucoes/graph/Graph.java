cb5c20af4cc97406227787a856192268b1e00cce
Maria Rayane Alves <mrayalves05@gmail.com>
2019-06-24 09:55:54 -0500

    Comentando métodos da classe Graph

--- src/main/java/br/ufrn/imd/reordenamento/instrucoes/graph/Graph.java
+++ src/main/java/br/ufrn/imd/reordenamento/instrucoes/graph/Graph.java
@@ -2,12 +2,30 @@ package br.ufrn.imd.reordenamento.instrucoes.graph;
 
 import java.util.*;
 
+/**
+ * Classe que repesenta um grafo de instruções
+ * A ideia é que haja um nó de instrução e que
+ * cada nó seja filho (dependent) do nó com a instrução da qual ele depende para ser executado.
+ * É importante mencionar que uma instrução pode depender de, no máximo, duas outras instruções.
+ * Isto é, um nó pode ter, no máximo, dois nós responsáveis.
+ */
+
 public class Graph {
+	// Nó raíz
 	private Node root;
+
+	// Nível do nó
 	private Integer level;
+
+	// Responsáveis do nó
 	private List<Graph> responsables;
+
+	// Dependentes do nó
 	private List<Graph> dependents;
-	
+
+	/**
+	 * Construtores
+	 */
 	public Graph() {
 		this.root = null;
 		this.dependents = new ArrayList<Graph>();
@@ -29,6 +47,10 @@ public class Graph {
 		this.responsables = new ArrayList<Graph>();
 	}
 
+	/**
+	 * Geters e Seters
+	 *
+	 */
 	public Node getRoot() {
 		return root;
 	}
@@ -63,29 +85,44 @@ public class Graph {
 	 */
 	public List<String> returnGraphDependences(){
 		List<String> orderedInstructions = new ArrayList<String>();
+
+		// Percorre o grafo por nível
 		List<Graph> graphLevelOrdered = levelOrder();
 
 		String compactString = "";
 
+		// Se houver algo no grafo
 		if (graphLevelOrdered != null){
+
 			for (int i = 0; i < graphLevelOrdered.size(); i++){
+				// Se o nó for difrente de nulo
 				if (graphLevelOrdered.get(i).getRoot() != null){
 					compactString += i + " " + graphLevelOrdered.get(i).getRoot().toString() + " | ";
-					
+
+					// Se o nó tiver responsáveis diferente de nulo
 					if (graphLevelOrdered.get(i).getResponsables() != null){
+
+						// Se não for vazio
 						if (!graphLevelOrdered.get(i).getResponsables().isEmpty()){
+
+							// Busca qual posição da lita está o responsável e adiciona na strig construída naquele nó
 							compactString += findInGraph(graphLevelOrdered, graphLevelOrdered.get(i).getResponsables().get(0)) + " ";
 
+							// Se tiver dois responsáveis
 							if (graphLevelOrdered.get(i).getResponsables().size() == 2){
 								compactString += findInGraph(graphLevelOrdered, graphLevelOrdered.get(i).getResponsables().get(1)) + " ";
 							}
 						}
+
 					}
-					
+
+					// Adiciona a string constrída
 					orderedInstructions.add(compactString);
 					compactString = "";
 				}
+
 			}
+
 		}
 
 		return orderedInstructions;
@@ -96,7 +133,7 @@ public class Graph {
 	 * @param newNode Node a ser inserido
 	 */
     public void insertNode(Node newNode) {
-        // Se o grafo não tiver nenhum dependente, apenas insere o novo nó
+    	// Se o grafo não tiver nenhum dependente, apenas insere o novo nó
         if (this.dependents.isEmpty()) {
             this.dependents.add(new Graph(newNode, 2));
             return;
@@ -113,6 +150,7 @@ public class Graph {
 
         while (it.hasNext()) {
             Graph graph = (Graph) it.next();
+
             // Se a raiz do novo grafo for dependente do grafo iterado
             if (newGraph.getRoot().isDependent(graph.getRoot())) {
                 newGraph.setResponsable(graph);
@@ -125,18 +163,18 @@ public class Graph {
                 } else {
                     newGraph.setLevel(higherLevel(newGraph.getResponsables()) + 1);
                 }
-
                 graph.setDependent(newGraph);
-            } else {
-            	if (graph.getRoot() != null) {
-            		if (graph.getRoot().isDependent(newGraph.getRoot())) {
-        				newGraph.setDependent(graph);
-        				graph.setResponsable(newGraph);
-        			}
-				}
+            }
+
+            // Se o grafo iterado for diferente de nulo e for dependente do novo grafo
+            else if ((graph.getRoot() != null) && (graph.getRoot().isDependent(newGraph.getRoot()))) {
+				newGraph.setDependent(graph);
+				graph.setResponsable(newGraph);
             }
         }
 
+        // Se depois de percorrer o grafo completo o novo grafo não encontrar nenhuma dependência para ele, insere ele
+		// no primeiro nível
         if (!hasResponsable){
             this.dependents.add(new Graph(newNode, 2));
         }
@@ -167,36 +205,64 @@ public class Graph {
 		List<Graph> graphs = new ArrayList<Graph>();
 
 		Graph graph = this;
+
+		// Emplilha o grafo
 		queue.enqueue(graph);
-		
+
+		// Enquanto a pilha estiver com algum elemento
 		while(!queue.isEmpty()) {
+			// Despilha
 			graph = queue.dequeue();
+
+			// Adiciona esse grafo despilhado
 			graphs.add(graph);
-			
+
+			// Se houver dependentes
 			if ((graph.dependents != null) && (!graph.dependents.isEmpty())){
+
 				for (Graph dependent : graph.getDependents()) {
+
+					// Se o nível do dependente for o imediatamente abaixo do responsável
 					if (dependent.getLevel() == graph.getLevel() + 1) {
 						queue.enqueue(dependent);
 					}
+
 				}
+
 			}
+
 		}
 
 		return graphs;
 	}
 
+	/**
+	 * Verifica se um grafo é igual ao outro
+	 * @param graph Graph a ser comparado
+	 * @return boolean respondendo se é ou não
+	 */
 	private boolean equals(Graph graph){
 		boolean equals = false;
 
+		// Se as raizes não forem nulas
 		if ((this.root != null) && (graph.root != null)){
+
+			// Se as raízes forem iguais
 			if (this.root.equals(graph.root)){
 				equals = true;
 			}
+
 		}
 
 		return equals;
 	}
-	
+
+	/**
+	 * Busca a posição de um grafo em uma lista
+	 * @param graphs List de grafos
+	 * @param graph Graph a ser buscado
+	 * @return Integer com a posiçao do grafo na lista
+	 */
 	private Integer findInGraph(List<Graph> graphs, Graph graph) {
 		Integer position = -1;
 		
